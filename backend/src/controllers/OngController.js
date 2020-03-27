9f66990b4cf40f3b8711f65549c88728bd096109
augustoplima <augusto.lima442@gmail.com>
2020-03-27 15:23:50 -0300

    Update OngController ongid

--- backend/src/controllers/OngController.js
+++ backend/src/controllers/OngController.js
@@ -1,12 +1,12 @@
 const connection = require('../database/connection');
-const crypto = require('crypto');
+const generateUniqueId =  require('../utils/generateUniqueId');
 module.exports ={
     async create(request,response){
         
         //uf => federation unit
         const {name,email,whatsapp,city,uf} = request.body;
         
-        const id  = crypto.randomBytes(4).toString('HEX');
+        const id  = generateUniqueId();
         
         await connection('ongs').insert({
             id,
